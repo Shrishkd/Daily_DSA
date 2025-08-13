532f0f9aae31c63af9ccb3b76504e57bd3805578
Shrishkd <shrishdas444@gmail.com>
2025-08-13 23:15:18 +0530

    Stack Added

--- Stack/Implemention.cpp
+++ Stack/Implemention.cpp
@@ -2,9 +2,100 @@
 #include <stack>
 using namespace std;
 
+class Stack{
+    public:
+
+        int *arr;
+        int top;
+        int size;
+
+    Stack(int size){
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element){
+        if(size - top > 1){
+            top++;
+            arr[top] = element;
+        }
+        else{
+            cout<< "Stack Overflow"<<endl;
+        }
+
+    }
+
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout<<"Satck Underflow"<<endl;;
+        }
+    }
+
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }
+        else{
+            cout<<"Empty is empty"<<endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
 int main(){
 
-    //Creationn of stack
+
+    // Array implementation
+
+    Stack st(5);
+
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(22);
+    st.push(43);
+    st.push(44);
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    if(st.isEmpty()) {
+        cout << "Stack to Empty hai Laadle " << endl;
+    }
+    else{
+        cout << "Stack Empty nahi hai Laadle " << endl;
+    }
+
+
+    /* 
+
+        Using STL
+
+    //Creation of stack
     stack <int> s;
 
     //Insert Element
@@ -25,5 +116,7 @@ int main(){
     }
 
     cout<<"Size of element: " << s.size();
+
+    */
 
 }
