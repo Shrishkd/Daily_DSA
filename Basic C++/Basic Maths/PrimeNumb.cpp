43312fc235cbf9c11a7d010a4ae82b811df11454
Shrish <shrishdas444@gmail.com>
2026-07-16 15:49:36 +0530

    prime number

---
+++ Basic C++/Basic Maths/PrimeNumb.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool isPrime(int x){
+
+    if(x<=1) return false;
+    for(int i =2; i<=sqrt(x); i++){
+        if(x%i==0){
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+int main(){
+
+    int n = 5;
+
+    if(isPrime(n)) cout <<"Prime number"<<endl;
+    else cout <<"Not a Prime Number"<<endl;
+
+    return 0;
+
+}
