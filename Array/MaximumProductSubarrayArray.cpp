789853e23c2aa6fd1cc99dc0ff0aea5d91c3177e
Shrish <shrishdas444@gmail.com>
2026-09-04 18:51:46 +0530

    added brute force approach as well

--- Array/MaximumProductSubarrayArray.cpp
+++ Array/MaximumProductSubarrayArray.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution{
 public:
-
+    //Opitmal Approach
     int maxProduct(vector<int>&nums){
 
         int n = nums.size();
@@ -29,15 +29,55 @@ public:
     }
 };
 
+// Brute Force:
+
+int maxProd(vector<int>&nums){
+
+    
+    if(nums.size()==1){
+        return nums[0];
+    }
+
+    int product = INT_MIN;
+
+    for(int i = 0; i<nums.size(); i++ ){
+
+        int prod = nums[i];
+
+        if(i==nums.size()-1){
+            product = max(product, prod);
+        }else{
+            for(int j = i+1; j<nums.size(); j++){
+            
+                product = max(product, prod);
+
+                prod *= nums[j];
+
+                product = max(product, prod);
+
+            }
+        }
+
+    }
+    return product;
+
+}
+
+
+
 int main(){
 
     vector<int> nums = {1,0,-5,2,3,-8,-9};
 
     Solution obj;
 
-    int ans = obj.maxProduct(nums);
+    int ans1 = obj.maxProduct(nums);
 
-    cout<<ans;
+    int ans2 = maxProd(nums);
 
-    return ans;
+    cout<<"OPtimal: "<<ans1<<endl;
+
+    cout<<"Brute: "<<ans2<<endl;
+
+    return 0;
 }
